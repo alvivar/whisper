34b36a81eb4016a8325a2dac03a6e6085734c342
alvivar <andresalvivar@gmail.com>
2015-03-03 21:05:07 -0600

    Whisper API

---
+++ app/api/controllers/WhisperController.js
@@ -0,0 +1,15 @@
+/**
+ * WhisperController
+ *
+ * @description :: Server-side logic for managing whispers
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+    hi: function(req, res) {
+        return res.send("hi!");
+    },
+    bye: function(req, res) {
+        return res.send("bye.")
+    }
+};
