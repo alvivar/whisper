1728c54e1990aca52bbef74980c95a4c01a52abe
alvivar <andresalvivar@gmail.com>
2019-06-01 22:40:40 -0600

    Working mutation

--- client/src/components/CreatePost.js
+++ client/src/components/CreatePost.js
@@ -3,50 +3,55 @@ import { Mutation } from "react-apollo";
 import gql from "graphql-tag";
 
 const POST_MUTATION = gql`
-  mutation CreateDraftMutation($content: String!, $userId: ID!) {
-    createDraft(content: $content, userId: $userId) {
-      id
+    mutation CreateDraftMutation($content: String!, $userId: ID!) {
+        createDraft(content: $content, userId: $userId) {
+            id
+        }
     }
-  }
 `;
 
 class CreatePost extends Component {
-  state = {
-    content: "",
-    userId: "cjw7b3mmd004407271ba3o6p1"
-  };
+    state = {
+        content: "",
+        userId: "cjw64p0od00do07713xhr0esc"
+    };
 
-  render() {
-    let textArea;
-    const { content, userId } = this.state;
+    render() {
+        let textArea;
+        const { content, userId } = this.state;
 
-    return (
-      <div>
-        <div className="flex">
-          <Mutation mutation={POST_MUTATION}>
-            <textarea
-              ref={node => (textArea = node)}
-              onChange={e => this.setState({ content: e.target.value })}
-              className="w-10/12 m-1 bg-gray-100 text-gray-800 border border-gray-200 focus:bg-white rounded"
-            />
-            {(postMutation, { loading, error, data }) => (
-              <button
-                onClick={e => {
-                  postMutation({
-                    variables: { content: content, userId: userId }
-                  });
-                  textArea.value = "";
-                }}
-                className="w-2/12 m-1 bg-transparent hover:bg-blue-400 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
-              >
-                WHISPER
-              </button>
-            )}
-          </Mutation>
-        </div>
-      </div>
-    );
-  }
+        return (
+            <div>
+                <div className="flex">
+                    <textarea
+                        ref={node => (textArea = node)}
+                        onChange={e =>
+                            this.setState({ content: e.target.value })
+                        }
+                        className="w-10/12 m-1 bg-gray-100 text-gray-800 border border-gray-200 focus:bg-white rounded"
+                    />
+                    <Mutation mutation={POST_MUTATION}>
+                        {(postMutation, { loading, error, data }) => (
+                            <button
+                                onClick={e => {
+                                    postMutation({
+                                        variables: {
+                                            content: content,
+                                            userId: userId
+                                        }
+                                    });
+                                    textArea.value = "";
+                                }}
+                                className="w-2/12 m-1 bg-transparent hover:bg-blue-400 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+                            >
+                                WHISPER
+                            </button>
+                        )}
+                    </Mutation>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default CreatePost;
