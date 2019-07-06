f2e8deabff9ef9ee89d2fb6016792a9050f8229d
alvivar <andresalvivar@gmail.com>
2019-07-06 16:49:18 -0600

    Set user name

--- client/src/components/CreatePost.js
+++ client/src/components/CreatePost.js
@@ -10,28 +10,61 @@ const POST_MUTATION = gql`
     }
 `;
 
-const CreatePost = userId => {
+const SET_USER_NAME_MUTATION = gql`
+    mutation setUserName($userId: ID!, $name: String!) {
+        setUserName(userId: $userId, name: $name) {
+            id
+            name
+        }
+    }
+`;
+
+const CreatePost = ({ userId, userName }) => {
+    let [name, setName] = useState(userName);
     let [content, setContent] = useState("");
-    let [textAreaRef, setTextAreaRef] = useState();
+    let [inputField, setInputField] = useState();
+    let [textArea, setTextArea] = useState();
+
     const createPostMutation = useMutation(POST_MUTATION);
+    const setUserNameMutation = useMutation(SET_USER_NAME_MUTATION);
 
     return (
         <div className="flex flex-wrap">
+            {name}
+            <div className="w-full mb-2">
+                <input
+                    ref={node => setInputField(node)}
+                    className="w-2/3 py-2 px-2 float-right text-right text-gray-900 border-transparent bg-gray-100 outline-none rounded-lg focus:bg-gray-300"
+                    onChange={e => {
+                        setUserNameMutation({
+                            variables: {
+                                userId: userId,
+                                name: e.target.value
+                            }
+                        });
+                        setName(e.target.value);
+                    }}
+                    value={name}
+                />
+            </div>
             <textarea
-                ref={node => setTextAreaRef(node)}
-                onChange={e => setContent(e.target.value)}
+                ref={node => setTextArea(node)}
                 className="w-full h-16 py-4 px-4 text-lg text-gray-900 border bg-gray-100 border-gray-100 outline-none rounded-lg focus:bg-gray-300"
+                onChange={e => setContent(e.target.value)}
+                value={content}
             />
             <div className="w-full">
                 <button
                     onClick={e => {
-                        createPostMutation({
-                            variables: {
-                                content: content,
-                                userId: userId
-                            }
-                        });
-                        textAreaRef.value = "";
+                        if (userId) {
+                            createPostMutation({
+                                variables: {
+                                    content: content,
+                                    userId: userId
+                                }
+                            });
+                            textArea.value = "";
+                        }
                     }}
                     className="my-2 h-12 mx-4 py-2 px-4 float-right text-gray-600 outline-none hover:bg-black hover:text-white bg-transparent border-transparent rounded-lg"
                 >
