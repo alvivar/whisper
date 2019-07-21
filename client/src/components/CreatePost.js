229139a9be6a64c50f3361118983cecc11d039e0
alvivar <andresalvivar@gmail.com>
2019-07-20 21:47:27 -0600

    Values debounced and trimmed

--- client/src/components/CreatePost.js
+++ client/src/components/CreatePost.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
 import gql from "graphql-tag";
 import { useMutation } from "react-apollo-hooks";
+
 import useKeyPress from "../hooks/useKeyPress";
+import useDebounce from "../hooks/useDebounce";
 
 const POST_MUTATION = gql`
     mutation createDraft($content: String!, $userId: ID!) {
@@ -21,29 +23,56 @@ const SET_USER_NAME_MUTATION = gql`
 `;
 
 const CreatePost = ({ userId, userName, postsRefetch }) => {
-    const [name, setName] = useState(userName);
-    const [content, setContent] = useState("");
-    const [textArea, setTextArea] = useState();
-    const enterPress = useKeyPress("Enter");
-
     const createPostMutation = useMutation(POST_MUTATION);
     const setUserNameMutation = useMutation(SET_USER_NAME_MUTATION);
 
+    const [name, setName] = useState(userName);
+    const [content, setContent] = useState("");
+    const [textArea, setTextArea] = useState();
+
+    const debouncedNewName = useDebounce(name, 1000);
+
+    const ctrlKeyDown = useKeyPress("Control");
+    const enterKeyDown = useKeyPress("Enter");
+
+    const createPost = async (userId, content) => {
+        await createPostMutation({
+            variables: {
+                content: content,
+                userId: userId
+            }
+        });
+
+        setContent("");
+        textArea.focus();
+        postsRefetch();
+    };
+
     useEffect(() => {
-        if (enterPress && userName !== name) {
+        console.log("Trimming name");
+        if (!name.trim()) setName(userName);
+    }, [name, userName]);
+
+    useEffect(() => {
+        console.log("New name detected");
+        if (debouncedNewName && debouncedNewName !== userName) {
+            console.log("Saving new name");
             setUserNameMutation({
                 variables: {
                     userId: userId,
-                    name: name
+                    name: debouncedNewName
                 }
             });
         }
-    }, [userId, userName, name, enterPress, setUserNameMutation]);
+    }, [debouncedNewName]);
 
     useEffect(() => {
-        console.log("Trimming name...");
-        if (!name.trim()) setName(userName);
-    }, [userName]);
+        console.log("Detecting Ctrl + Enter");
+        if (ctrlKeyDown && enterKeyDown) {
+            console.log("Ctrl + Enter pressed");
+            createPost(userId, content);
+        }
+    }, [ctrlKeyDown, enterKeyDown]);
 
     return (
         <div className="flex flex-wrap">
@@ -63,22 +92,7 @@ const CreatePost = ({ userId, userName, postsRefetch }) => {
             <div className="w-full">
                 <button
                     onClick={e => {
-                        if (userId) {
-                            const updateAfterMutation = async () => {
-                                await createPostMutation({
-                                    variables: {
-                                        content: content,
-                                        userId: userId
-                                    }
-                                });
-
-                                postsRefetch();
-                                setContent("");
-                                textArea.focus();
-                            };
-
-                            updateAfterMutation();
-                        }
+                        if (userId) createPost(userId, content);
                     }}
                     className="my-2 h-12 mx-4 py-2 px-4 float-right text-gray-600 outline-none hover:bg-black hover:text-white bg-transparent border-transparent rounded-lg"
                 >
