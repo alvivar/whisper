209e1653a16586d5916cb44ad3a553b542fccce3
Andrés Villalobos <andresalvivar@gmail.com>
2019-07-22 18:16:57 -0600

    Transparent

--- client/src/components/CreatePost.js
+++ client/src/components/CreatePost.js
@@ -6,100 +6,100 @@ import useKeyPress from "../hooks/useKeyPress";
 import useDebounce from "../hooks/useDebounce";
 
 const POST_MUTATION = gql`
-    mutation createDraft($content: String!, $userId: ID!) {
-        createDraft(content: $content, userId: $userId) {
-            id
-        }
+  mutation createDraft($content: String!, $userId: ID!) {
+    createDraft(content: $content, userId: $userId) {
+      id
     }
+  }
 `;
 
 const SET_USER_NAME_MUTATION = gql`
-    mutation setUserName($userId: ID!, $name: String!) {
-        setUserName(userId: $userId, name: $name) {
-            id
-            name
-        }
+  mutation setUserName($userId: ID!, $name: String!) {
+    setUserName(userId: $userId, name: $name) {
+      id
+      name
     }
+  }
 `;
 
 const CreatePost = ({ userId, userName, postsRefetch }) => {
-    const createPostMutation = useMutation(POST_MUTATION);
-    const setUserNameMutation = useMutation(SET_USER_NAME_MUTATION);
+  const createPostMutation = useMutation(POST_MUTATION);
+  const setUserNameMutation = useMutation(SET_USER_NAME_MUTATION);
 
-    const [room, setRoom] = useState("universe");
-    const [name, setName] = useState(userName);
-    const [content, setContent] = useState("");
-    const [textArea, setTextArea] = useState();
+  const [room, setRoom] = useState("universe");
+  const [name, setName] = useState(userName);
+  const [content, setContent] = useState("");
+  const [textArea, setTextArea] = useState();
 
-    const debouncedNewName = useDebounce(name, 1000);
+  const debouncedNewName = useDebounce(name, 1000);
 
-    const ctrlKeyDown = useKeyPress("Control");
-    const enterKeyDown = useKeyPress("Enter");
+  const ctrlKeyDown = useKeyPress("Control");
+  const enterKeyDown = useKeyPress("Enter");
 
-    const createPost = async (userId, content) => {
-        await createPostMutation({
-            variables: {
-                content: content.trim(),
-                userId: userId
-            }
-        });
+  const createPost = async (userId, content) => {
+    await createPostMutation({
+      variables: {
+        content: content.trim(),
+        userId: userId
+      }
+    });
 
-        setContent("");
-        textArea.focus();
-    };
+    setContent("");
+    textArea.focus();
+  };
 
-    useEffect(() => {
-        console.log("Trimming name");
-        if (!name.trim()) setName(userName);
-    }, [name, userName]);
+  useEffect(() => {
+    console.log("Trimming name");
+    if (!name.trim()) setName(userName);
+  }, [name, userName]);
 
-    useEffect(() => {
-        console.log("New name detected");
-        if (debouncedNewName && debouncedNewName !== userName) {
-            console.log("Saving new name");
-            setUserNameMutation({
-                variables: {
-                    userId: userId,
-                    name: debouncedNewName
-                }
-            });
+  useEffect(() => {
+    console.log("New name detected");
+    if (debouncedNewName && debouncedNewName !== userName) {
+      console.log("Saving new name");
+      setUserNameMutation({
+        variables: {
+          userId: userId,
+          name: debouncedNewName
         }
-    }, [debouncedNewName]);
+      });
+    }
+  }, [debouncedNewName]);
 
-    useEffect(() => {
-        console.log("Detecting Ctrl + Enter");
-        if (ctrlKeyDown && enterKeyDown) {
-            console.log("Ctrl + Enter pressed");
-            createPost(userId, content);
-        }
-    }, [ctrlKeyDown, enterKeyDown]);
+  useEffect(() => {
+    console.log("Detecting Ctrl + Enter");
+    if (ctrlKeyDown && enterKeyDown) {
+      console.log("Ctrl + Enter pressed");
+      createPost(userId, content);
+    }
+  }, [ctrlKeyDown, enterKeyDown]);
 
-    return (
-        <div className="flex flex-wrap">
-            <input
-                className="float-right w-full p-1 my-2 text-right text-gray-600 focus:text-gray-800 bg-blue-100 focus:bg-blue-200 border-transparent outline-none rounded-lg"
-                onChange={e => setName(e.target.value)}
-                value={name}
-            />
-            <textarea
-                ref={node => setTextArea(node)}
-                className="w-full h-32 py-4 px-4 text-lg text-gray-800 border bg-blue-100 focus:bg-blue-200 border-gray-100 outline-none rounded-lg"
-                onChange={e => setContent(e.target.value)}
-                value={content}
-            />
-            <div className="w-full">
-                <button
-                    onClick={e => {
-                        if (userId) createPost(userId, content);
-                    }}
-                    className="float-right h-16 my-2 py-2 px-4 text-sm text-gray-500 hover:text-white outline-none bg-blue-100 hover:bg-blue-400 border-transparent rounded-lg"
-                >
-                    <span className="text-xl">whisper</span>{" "}
-                    <span className="text-xs">ctrl + enter</span>
-                </button>
-            </div>
-        </div>
-    );
+  return (
+    <div className="flex flex-wrap">
+      <input
+        className="float-right w-full p-1 my-2 text-right text-gray-600 focus:text-gray-800 bg-blue-100 focus:bg-blue-200 border-transparent outline-none rounded-lg"
+        onChange={e => setName(e.target.value)}
+        value={name}
+      />
+      <textarea
+        ref={node => setTextArea(node)}
+        className="w-full h-32 py-4 px-4 text-lg text-gray-800 border bg-blue-100 focus:bg-blue-200 border-transparent outline-none rounded-lg"
+        onChange={e => setContent(e.target.value)}
+        value={content}
+      />
+      <div className="w-full">
+        <button
+          onClick={e => {
+            if (userId) createPost(userId, content);
+          }}
+          className="float-right h-16 my-2 py-2 px-4 text-sm text-gray-500 hover:text-white outline-none bg-blue-100 hover:bg-blue-400 border-transparent rounded-lg"
+        >
+          <span className="text-xl">whisper</span>{" "}
+          <span className="text-xs">ctrl + enter</span>
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default CreatePost;
