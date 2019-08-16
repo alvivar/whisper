48301b9b39da71da83461a13223ff093f46d9dea
alvivar <andresalvivar@gmail.com>
2019-08-15 21:49:39 -0600

    Script that deletes users without posts

---
+++ server/scripts/deleteUsersWithoutPosts.js
@@ -0,0 +1,29 @@
+const { prisma } = require("../generated/prisma-client");
+
+// Remove users without written posts.
+async function main() {
+    const users = await prisma.users().$fragment(`
+        fragment UsersWithoutPosts on User {
+            id
+            name
+            writtenPosts {
+                id
+            }
+        }`);
+
+    const usersWithoutPosts = users.reduce((accu, elem) => {
+        if (!elem.writtenPosts.length) accu.push(elem.id);
+        return accu;
+    }, []);
+
+    await prisma.deleteManyUsers({
+        id_in: usersWithoutPosts
+    });
+
+    console.log(usersWithoutPosts);
+    console.log(`Users without posts deleted!`);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch(e => console.error(e));
