d3a0dc09b9fe42b9f7945708305a49a7f3a3397f
alvivar <andresalvivar@gmail.com>
2019-08-17 15:36:47 -0600

    More sync stuff

--- scripts/sync-server.sh
+++ scripts/sync-server.sh
@@ -1,1 +1,7 @@
+
+# Sync whisper with the Droplet
 rsync -rave ssh --exclude-from=whisper/exclude.txt whisper/ --delete root@165.22.45.96:~/whisper
+
+# Sync NGINX default with the server
+rsync -rave ssh root@165.22.45.96:/etc/nginx/sites-available/default whisper/nginx/
+rsync -rave ssh whisper/nginx/ root@165.22.45.96:/etc/nginx/sites-available/
