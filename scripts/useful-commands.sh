eb90844a3fd03cf58092ef2235ded804bde2fae0
alvivar <andresalvivar@gmail.com>
2019-08-17 20:45:55 -0600

    Docs

--- scripts/useful-commands.sh
+++ scripts/useful-commands.sh
@@ -2,8 +2,9 @@
 # Sync whisper with the Droplet
 rsync -rave ssh --exclude-from=whisper/exclude.txt whisper/ --delete root@165.22.45.96:~/whisper
 
-# Sync NGINX default with the server
+# From server to local
 rsync -rave ssh root@165.22.45.96:/etc/nginx/sites-available/default whisper/nginx/
+# Local to server
 rsync -rave ssh whisper/nginx/ root@165.22.45.96:/etc/nginx/sites-available/
 
 # Restart Nginx
