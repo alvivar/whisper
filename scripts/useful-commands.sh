1edd62e45c566bbeaab5d95c5486a4a697486825
alvivar <andresalvivar@gmail.com>
2019-08-17 18:04:37 -0600

    Better name

--- scripts/useful-commands.sh
+++ scripts/useful-commands.sh
@@ -5,3 +5,6 @@ rsync -rave ssh --exclude-from=whisper/exclude.txt whisper/ --delete root@165.22
 # Sync NGINX default with the server
 rsync -rave ssh root@165.22.45.96:/etc/nginx/sites-available/default whisper/nginx/
 rsync -rave ssh whisper/nginx/ root@165.22.45.96:/etc/nginx/sites-available/
+
+# Restart Nginx
+systemctl restart nginx
