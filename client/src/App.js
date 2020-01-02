850105e8b6519f31b2848a7b15b47ba388dfa499
Andrés Villalobos <andresalvivar@gmail.com>
2020-01-01 18:26:18 -0600

    Sexier windows tooling

--- client/src/App.js
+++ client/src/App.js
@@ -7,23 +7,20 @@ import './styles/tailwind.css'
 
 import useDebounce from './hooks/useDebounce'
 
-const BlogName = ({ name }) => {
-  return <InputField Value={name}></InputField>
-}
-
-const InputField = ({ defaultValue, SaveValue }) => {
-  const inputBgOk = 'bg-blue-100 focus:bg-blue-200'
-  const inputBgEditing = 'bg-green-100 focus:bg-green-200'
-  const inputBgError = 'bg-red-300 focus:bg-red-400'
-  const [inputBg, setInputBg] = useState(inputBgOk)
-
+const InputField = ({ defaultValue, OnValueChange }) => {
   const [value, setValue] = useState(defaultValue)
   const debouncedValue = useDebounce(value, 1000)
+
+  const bgOk = 'bg-blue-100 focus:bg-blue-200'
+  const bgEditing = 'bg-green-100 focus:bg-green-200'
+  const bgError = 'bg-red-300 focus:bg-red-400'
+  const [bg, setBg] = useState(bgOk)
 
   useEffect(() => {
     console.log('InputField modified')
     if (debouncedValue) {
-      SaveValue(debouncedValue)
+      OnValueChange(debouncedValue)
+      setBg(bgOk)
       console.log(`InputField saved: ${debouncedValue}`)
     }
   }, [debouncedValue])
@@ -31,10 +28,10 @@ const InputField = ({ defaultValue, SaveValue }) => {
   return (
     <div>
       <input
-        className={`float-right w-full p-1 my-2 text-gray-600 focus:text-gray-800 ${inputBg} border-transparent outline-none rounded-lg`}
+        className={`w-full py-4 px-4 my-2 text-gray-600 focus:text-gray-800 ${bg} border-transparent outline-none rounded-lg`}
         onChange={e => {
           setValue(e.target.value)
-          setInputBg(inputBgEditing)
+          setBg(bgEditing)
         }}
         value={value}
       />
@@ -42,26 +39,31 @@ const InputField = ({ defaultValue, SaveValue }) => {
   )
 }
 
-const TextArea = ({ defaultValue, SaveValue }) => {
-  const textAreaBgOk = 'bg-blue-100 focus:bg-blue-200'
-  const textAreaBgError = 'bg-red-300 focus:bg-red-400'
-  const [textAreaBg, setTextAreaBg] = useState(textAreaBgOk)
-
+const TextArea = ({ defaultValue, OnValueChange }) => {
   const [value, setValue] = useState(defaultValue)
   const debouncedValue = useDebounce(value, 1000)
+
+  const bgOk = 'bg-blue-100 focus:bg-blue-200'
+  const bgEditing = 'bg-green-100 focus:bg-green-200'
+  const bgError = 'bg-red-300 focus:bg-red-400'
+  const [bg, setBg] = useState(bgOk)
 
   useEffect(() => {
     console.log('TextArea modified')
     if (debouncedValue) {
-      SaveValue(debouncedValue)
+      OnValueChange(debouncedValue)
+      setBg(bgOk)
       console.log(`TextArea saved: ${debouncedValue}`)
     }
   }, [debouncedValue])
 
   return (
     <textarea
-      className={`w-full h-32 py-4 px-4 text-gray-800 ${textAreaBg} border border-transparent outline-none rounded-lg`}
-      onChange={e => setValue(e.target.value)}
+      className={`w-full h-32 py-4 px-4 my-2 text-gray-800 ${bg} border-transparent outline-none rounded-lg`}
+      onChange={e => {
+        setValue(e.target.value)
+        setBg(bgEditing)
+      }}
       value={value}
     />
   )
@@ -72,9 +74,9 @@ function App () {
   const [postContent, setPostContent] = useState('Post content')
 
   return (
-    <div>
-      <InputField Value={blogName} SetValue={setBlogName}></InputField>
-      <TextArea Value={postContent} SetValue={setPostContent}></TextArea>
+    <div className='container mx-auto max-w-xl'>
+      <InputField Value={blogName} OnValueChange={setBlogName}></InputField>
+      <TextArea Value={postContent} OnValueChange={setPostContent}></TextArea>
     </div>
   )
 }
